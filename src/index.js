f1e250c8be9cdfd1d5ff445b85b1e40ad4473ba2
Chris Russell <crussell@sonnysdirect.com>
2018-09-19 02:40:29 -0400

    Introduce transcoders for pluggable encode/decode routines

--- src/index.js
+++ src/index.js
@@ -9,7 +9,84 @@
 const {EventEmitter} = require('events');
 const net = require('net');
 const fs = require('fs');
-const delimiter = '%<EOM>%\n';
+const jsonTranscoder = require('./lib/transcoder/jsonTranscoder');
+const {MessageError} = require('./lib/error');
+
+/**
+ * @interface MessageWrapper
+ *
+ * @property {string} topic - A non-empty topic for the message.
+ * @property {*} message - The message. This may be any value that can be encoded by the current Transcoder. Unless a
+ *     custom Transcoder is configured, this must be a JSON serializable object.
+ */
+
+/**
+ * @interface Transcoder
+ *
+ * @property {string} socketEncoding - The encoding to use when reading/writing data on the underlying socket.
+ * @property {encoderFactoryFunc} createEncoder - A no-argument factory function which returns an `encoderFunc`. The
+ *     returned `encoderFunc` may be used to encode across multiple connections making it impossible to predict which
+ *     encoder instance is used for which connection. For this reason, attempting to make an encoder which "buffers"
+ *     data from several messages before sending it will result in undefined behavior.
+ * @property {decoderFactoryFunc} createDecoder - A no-argument factory function which returns a `decoder`. Each unique
+ *     connection is guaranteed to receive its own decoder instance. In most cases, each returned decoder should
+ *     contain some sort of stateful "buffer" to handle cases where a message's data is spread across multiple
+ *     encoder calls.
+ */
+
+/**
+ * @callback encoderFactoryFunc
+ * @returns {encoderFunc}
+ */
+
+/**
+ * @callback decoderFactoryFunc
+ * @returns {decoderFunc}
+ */
+
+/**
+ * Invoked by an `encoderFunc` when it has finished its work.
+ *
+ * The `EncoderFunc` MUST invoke this callback when it is done working with either the first or second argument
+ * populated.
+ *
+ * @callback encodedCallback
+ * @param {Error, EncodeError} error - If an error occurred, an `Error` should be passed as the first arg.
+ * @param {*} data - The encoded data. The encoder MAY use `null` as a data value to indicate that the message was
+ *     skipped; no data will be sent in this case and the message will be silently discarded. The return value must
+ *     be ready to be written to the `net.Socket` so it should be in agreement with the socket encoding set by
+ *     `Transcoder#socketEncoding`.
+ */
+
+/**
+ * Invoked by an `decoderFunc` when it has finished its work.
+ *
+ * The `EncoderFunc` MUST invoke this callback when it is done working with either the first or second argument
+ * populated.
+ *
+ * @callback decodedCallback
+ * @param {Error, DecodeError} error - If an error occurred, an `Error` should be passed as the first arg.
+ * @param {MessageWrapper[]} data - An array `MessageWrapper` objects that each include a single message and its topic.
+ *     This may be an empty array in cases where the `decoderFunc` did not receive enough data for a complete message.
+ */
+
+/**
+ * Encodes a `MessageWrapper` so that it can be placed on the socket.
+ *
+ * @callback encoderFunc
+ * @param {MessageWrapper} msgWrapper - The message to be encoded and its topic.
+ * @param {encodedCallback} callback - The callback to invoke after all work is complete.
+ */
+
+/**
+ * Decodes raw data.
+ *
+ * @callback decoderFunc
+ * @param {*} chunk - A chunk of data to be decoded. This may contain a full message, a partial message, or multiple
+ *     messages. The data type will depend on the socket encoding defined by `Transcoder#socketEncoding`.
+ * @param {decodedCallback} callback - The callback to invoke after all work is complete.
+ */
+
 
 /**
  * Takes in a value and make sure it looks like a reasonable socket file path.
@@ -32,68 +109,59 @@ const validateSocketFileOption = (value) => {
     }
 };
 
-const parseMessage = raw => {
+/**
+ * Factory method for listening to incoming data on an underlying socket.
+ *
+ * @param {Socket} socket - The socket to listen to.
+ * @param {EventEmitter} emitter - Where to emit events from.
+ * @param {Transcoder} transcoder - The transcoder to use.
+ * @param {int} [clientId] - Only relevant in the server context. The id of the client the socket is attached to.
+ */
+const attachDataListener = (socket, emitter, transcoder, clientId) => {
 
-    // Decode as JSON
-    let message;
-    try {
-         message = JSON.parse(raw);
-    } catch(cause) {
-        // Not able to parse as JSON. Tell the server to emit an error and move on.
-        throw new MessageError('Failed to parse as JSON', raw);
-    }
+    /** @type {decoderFunc} */
+    const decoder = transcoder.createDecoder();
+    const emitError = (err) => {
+        socket.destroy(err);
+        emitter.emit('error', err, clientId);
+    };
+    const emitMessage = (msgWrapper) => {
+        emitter.emit('message', msgWrapper.message, msgWrapper.topic, clientId);
+        emitter.emit(`message.${msgWrapper.topic}`, msgWrapper.message, clientId);
+    };
 
-    // It is valid JSON, but it is a valid message?
-    if (!message.topic) {
-        // No topic, not a valid message. Tell the server to emit an error and move on.
-        throw new MessageError('Invalid message structure.', raw);
-    }
+    socket.on('data', chunk => {
+        // Run the decoder with a callback that either emits an error or messages.
+        decoder(chunk, (err, msgWrappers) => {
+            if (err) {
+                emitError(err);
+                return;
+            }
 
-    return message;
+            for (let i = 0; i < msgWrappers.length; i++) {
+                try {
+                    emitMessage(msgWrappers[i]);
+                } catch (err) {
+                    // Emit the error and stop processing messages.
+                    emitError(err);
+                    return;
+                }
+            }
+        });
+    });
 };
-
-class MessageError extends Error {
-    constructor(errorMessage, receivedMessage) {
-        super(errorMessage);
-        this.message = receivedMessage;
-    }
-}
-
-const send = (socket, data, topic) => {
-    socket.write(JSON.stringify({
-        topic: topic || 'none',
-        data: data
-    }) + delimiter);
-};
-
-class MessageBuffer {
-    constructor() {
-        this.buffer = '';
-    }
-
-    data(chunk) {
-
-        // Use the buffer plus this chunk as the data that we need to process.
-        let data = this.buffer += chunk;
-
-        // Split on the delimiter to find distinct and complete messages.
-        let messages = data.split(delimiter);
-
-        // Pop the last element off of the message array. It is either an incomplete message
-        // or an empty string. Use it as the new buffer value.
-        this.buffer = messages.pop();
-
-        return messages;
-    }
-}
-
 
 class Server extends EventEmitter {
     /**
-     * @param {string} options.socketFile - Path to the socket file to use. 
+     * @param {string} options.socketFile - Path to the socket file to use.
+     * @param {Transcoder} [options.transcoder] - The transcoder to use to prepare messages to be written to the
+     *     underlying socket or to process data being read from the underlying socket.
      */
     constructor(options) {
         super();
+
+        this._transcoder = options.transcoder || jsonTranscoder;
+        this._encoder = this._transcoder.createEncoder();
 
         // See if the given socket file looks like a port. We don't support running the server on a port.
         let invalidSockFileReason = validateSocketFileOption(options.socketFile);
@@ -169,12 +237,11 @@ class Server extends EventEmitter {
         
         this._server.on('connection', socket => {
             
-            const buffer = new MessageBuffer();
             const id = this._nextClientId++;
             this._sockets.set(socket, id);
             this._clientLookup.set(id, socket);
 
-            socket.setEncoding('utf8');
+            socket.setEncoding(this._transcoder.socketEncoding);
             socket.on('close', () => {
                 this._sockets.delete(socket);
                 this._clientLookup.delete(id);
@@ -184,22 +251,7 @@ class Server extends EventEmitter {
             this.emit('connection', id);
 
             // Listen for messages on the socket.
-            socket.on('data', chunk => {
-                // Process each message.
-                buffer.data(chunk).forEach(raw => {
-                    let message;
-                    try {
-                        message = parseMessage(raw);
-                    } catch(e) {
-                        this.emit('messageError', e, id);
-                        return;
-                    }
-
-                    // Message events get emitted by the server.
-                    this.emit('message', message.data, message.topic, id);
-                    this.emit(`message.${message.topic}`, message.data, id);
-                });
-            });
+            attachDataListener(socket, this, this._transcoder, id);
         });
 
         this._server.listen(this._socketFile);
@@ -214,9 +266,17 @@ class Server extends EventEmitter {
     }
 
     broadcast(topic, message) {
-        // Broadcast the message to all known sockets. 
-        this._sockets.forEach((id, socket) => {
-            send(socket, message, topic);
+        // Encode once, send many
+        this._encoder({topic, message}, (err, data) => {
+            if (err) {
+                this.emit('error', err);
+                return;
+            }
+
+            // Broadcast the message to all known sockets.
+            this._sockets.forEach((id, socket) => {
+                socket.write(data)
+            });
         });
     }
 
@@ -226,18 +286,30 @@ class Server extends EventEmitter {
             return;
         }
 
-        send(this._clientLookup.get(clientId), message, topic);
+        const socket = this._clientLookup.get(clientId);
+        this._encoder({topic, message}, (err, data) => {
+            if (err) {
+                this.emit('error', err);
+            } else {
+                socket.write(data);
+            }
+        });
     }
 }
 
 class Client extends EventEmitter {
     /**
+     * @param {Transcoder} [options.transcoder] - The transcoder to use to prepare messages to be written to the
+     *     underlying socket or to process data being read from the underlying socket.
      * @param {int} [options.retryDelay=1000] - The number of milliseconds to wait between connection attempts.
      * @param {int} [options.reconnectDelay=100] - The number of milliseconds to wait before reconnecting.
      * @param {string} options.socketFile - The path to the socket file to use.
      */
     constructor(options) {
         super();
+
+        this._transcoder = options.transcoder || jsonTranscoder;
+        this._encoder = this._transcoder.createEncoder();
         
         // See if the given socket file looks like a port. We don't support running the server on a port.
         let invalidSockFileReason = validateSocketFileOption(options.socketFile);
@@ -262,7 +334,7 @@ class Client extends EventEmitter {
     _connect(isReconnect) {
         
         const socket = net.createConnection({path: this._socketFile});
-        socket.setEncoding('utf8');
+        socket.setEncoding(this._transcoder.socketEncoding);
 
         // Until a connection is established, handle errors as connection errors.
         const handleConnectError = (err) => {
@@ -297,23 +369,8 @@ class Client extends EventEmitter {
             });
         });
 
-        const buffer = new MessageBuffer();
-        socket.on('data', chunk => {
-                // Process each message.
-                buffer.data(chunk).forEach(raw => {
-                    let message;
-                    try {
-                        message = parseMessage(raw);
-                    } catch(e) {
-                        this.emit('messageError', e);
-                        return;
-                    }
-
-                    // Emit the events.
-                    this.emit('message', message.data, message.topic);
-                    this.emit(`message.${message.topic}`, message.data);
-                });
-        });
+        // Listen for data on the socket.
+        attachDataListener(socket, this, this._transcoder);
     }
 
     close() {
@@ -327,8 +384,15 @@ class Client extends EventEmitter {
     }
 
     send(topic, message) {
-        send(this._socket, message, topic);
+        this._encoder({topic, message}, (err, data) => {
+            if (err) {
+                this.emit('error', err);
+            } else {
+                this._socket.write(data);
+            }
+        });
     }
+
 }
 
 module.exports = {
